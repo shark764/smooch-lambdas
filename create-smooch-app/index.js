474d22778f2413e36048a96a91552aa29ba407b1
Irvin Sandoval <irvandoval@gmail.com>
2019-10-07 09:34:30 -0600

    DynamoDB integration for create-smooch-app and delete-smooch-app (#15)

--- create-smooch-app/index.js
+++ create-smooch-app/index.js
@@ -3,34 +3,72 @@
  **/
 
 const SmoochCore = require('smooch-core');
+const AWS = require('aws-sdk');
+const smooch = new SmoochCore({
+    keyId: 'act_5d0bde67cc95250010636aac',
+    secret: 'eVgMHuwGm_ysETik3KiPNKFJ0XOG0wTVkepouUGJ2s_c721Nophtqu6Mf36bc1ffkl52OgmF93AJyysOLuVCwg',
+    scope: 'account'
+});
+
+AWS.config.update({ region: process.env.AWS_REGION || 'us-east-1' });
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    var smooch = new SmoochCore({
-        keyId: 'act_5d0bde67cc95250010636aac',
-        secret: 'eVgMHuwGm_ysETik3KiPNKFJ0XOG0wTVkepouUGJ2s_c721Nophtqu6Mf36bc1ffkl52OgmF93AJyysOLuVCwg',
-        scope: 'account', // account, app, or appUser
-    });
+    const { tenantId } = event;
+    const webhookUrl = `https://${process.env.DOMAIN}-smooch-gateway.cxengagelabs.net/tenants/${tenantId}/smooch`;
 
-    const { props } = event;
-    const url = 'https://us-east-1-dev-smooch-gateway.cxengagelabs.net/webhook';
+    console.log('create-smooch-app' + JSON.stringify(event));
+    console.log('create-smooch-app' + JSON.stringify(process.env));
 
+    let newApp;
     try {
-        let newApp = await smooch.apps.create({name: "TEST LAMBDA"});
-        let webIntegration = await smooch.integrations.create({appId: newApp.app._id, props: props || {type: 'web'}});
-        let webhooks = await smooch.webhooks.create(newApp.app._id, { target: url, triggers: ['*']});
-    
-        return {
-            statusCode: 200,
-            body: {
-                    app: newApp.app, 
-                    integration: webIntegration.integration, 
-                    webhooks
-                  }
-        };
+        newApp = await smooch.apps.create({ name: tenantId });
     } catch (error) {
+        console.error(JSON.stringify(error));
+
         return {
             statusCode: 500,
-            body: JSON.stringify(error)
+            body: { message: `An Error has occurred trying to create an App for tenant ${tenantId}` }
         };
-    } 
+    }
+
+    let webhook;
+    try {
+        webhook = await smooch.webhooks.create(newApp.app._id, { target: webhookUrl, triggers: ['*', 'typing:appUser'], includeClient: true });
+    } catch (error) {
+        console.error(JSON.stringify(error));
+        return {
+            statusCode: 500,
+            body: { message: `An Error has occurred trying to create webhooks for tenant ${tenantId}` }
+        };
+    }
+
+    const params = {
+        TableName: 'us-east-1-dev-smooch',
+        Item: {
+            'tenant-id': tenantId,
+            id: newApp.app._id,
+            type: 'app',
+            'webhook-id': webhook.webhook._id
+        }
+    };
+
+    try {
+       await docClient.put(params).promise();
+    } catch (error) {
+        console.error(JSON.stringify(error));
+
+        return {
+            statusCode: 500,
+            body: { message: `An Error has occurred trying to save a record in DynamoDB for tenant ${tenantId}` }
+        };
+    }
+
+    return {
+        statusCode: 200,
+        body: {
+            app: newApp.app,
+            webhook
+        }
+    };
 };
--- delete-smooch-app/index.js
+++ delete-smooch-app/index.js
@@ -3,27 +3,54 @@
  **/
 
 const SmoochCore = require('smooch-core');
+const AWS = require('aws-sdk');
+const smooch = new SmoochCore({
+    keyId: 'act_5d0bde67cc95250010636aac',
+    secret: 'eVgMHuwGm_ysETik3KiPNKFJ0XOG0wTVkepouUGJ2s_c721Nophtqu6Mf36bc1ffkl52OgmF93AJyysOLuVCwg',
+    scope: 'account'
+});
+
+AWS.config.update({ region: process.env.AWS_REGION || 'us-east-1' });
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    var smooch = new SmoochCore({
-        keyId: 'act_5d0bde67cc95250010636aac',
-        secret: 'eVgMHuwGm_ysETik3KiPNKFJ0XOG0wTVkepouUGJ2s_c721Nophtqu6Mf36bc1ffkl52OgmF93AJyysOLuVCwg',
-        scope: 'account', // account, app, or appUser
-    });
+    const { tenantId, appId } = event;
+    const params = {
+        TableName: 'us-east-1-dev-smooch',
+        Key: {
+            'tenant-id': tenantId,
+            id: appId,
+        }
+    };
 
-    const { appId } = event;
+    console.log('delete-smooch-app' + JSON.stringify(event));
+    console.log('delete-smooch-app' + JSON.stringify(process.env));
 
     try {
-        let app = await smooch.apps.delete(appId);
-    
-        return {
-            statusCode: 200,
-            body: { appId, deleted: true }
-        };
+        await docClient.delete(params).promise();
     } catch (error) {
+        console.error(JSON.stringify(error));
+
         return {
             statusCode: 500,
-            body: {...error, deleted: false}
+            body: { message: `An Error has occurred trying to delete a record in DynamoDB for tenant ${tenantId} and appId ${appId}`, deleted: false }
         };
-    } 
+    }
+
+    try {
+        await smooch.apps.delete(appId);
+    } catch (error) {
+        console.error(JSON.stringify(error));
+
+        return {
+            statusCode: 500,
+            body: { message: `An Error has occurred trying to delete an app for tenant ${tenantId} and appId ${appId}`, deleted: false }
+        };
+    }
+
+    return {
+        statusCode: 200,
+        body: { message: `The app with for tenant ${tenantId} and appId ${appId} has been deleted successfully`, deleted: true }
+    };
+   
 };
