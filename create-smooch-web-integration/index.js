63928e5eaf34e420969db5f250ae053ec6056c4a
Irvin Sandoval <irvandoval@gmail.com>
2019-10-11 11:42:43 -0600

    add create-smooch-web-integration and delete-smooch-web-integration (#18)

---
+++ create-smooch-web-integration/index.js
@@ -0,0 +1,171 @@
+/**
+ * Lambda that creates an smooch web integration
+ **/
+
+const SmoochCore = require('smooch-core');
+const smooch = new SmoochCore({
+    keyId: 'act_5da0b5671c42610010a7f245',
+    secret: '6-cyVcqWS185_zvDFGITUPAz1gM1IggG286IKSoEQFZNuDRonU7SZDOv3wOWiFzeMLUUQOijZXeD2BxGY7hhFw',
+    scope: 'account'
+});
+const Joi = require('@hapi/joi');
+
+AWS.config.update({ region: process.env.AWS_REGION || 'us-east-1' });
+const docClient = new AWS.DynamoDB.DocumentClient();
+const bodySchema = Joi.object({
+    appId: Joi.string()
+        .required(),
+
+    prechatCapture: Joi.string()
+        .required()
+        .valid('name', 'email'),
+
+    name: Joi.string()
+        .required(),
+
+    description: Joi.string(),
+
+    brandColor: Joi.string(),
+
+    originWhiteList: Joi.array()
+        .items(Joi.string()),
+
+    businessName: Joi.string(),
+
+    businessIconUrl: Joi.string(),
+
+    fixedIntroPane: Joi.boolean(),
+
+    conversationColor: Joi.string(),
+
+    backgroundImageUrl: Joi.string(),
+
+    actionColor: Joi.string(),
+
+    displayStyle: Joi.string()
+        .valid('button', 'tab'),
+
+    buttonWidth: Joi.string(),
+
+    buttonHeight: Joi.string(),
+
+    buttonIconUrl: Joi.string()
+});
+const paramsSchema = Joi.object({
+    'tenant-id': Joi.string.guid()
+});
+
+exports.handler = async (event) => {
+    console.log('create-smooch-web-integration' , JSON.stringify(event));
+    console.log('create-smooch-web-integration' , JSON.stringify(process.env));
+
+    const { body, params } = event;
+    try {
+        await bodySchema.validateAsync(body);
+    } catch (error) {
+        console.error('Error: invalid body value ' + error.details[0].message);
+
+        return {
+            statusCode: 400,
+            body: { message: 'Error: invalid body value ' + error.details[0].message}
+        };
+    }
+
+    try {
+       await paramsSchema.validateAsync(params);
+    } catch(error){
+        console.error('Error: invalid params value ' + error.details[0].message);
+        
+        return {
+            statusCode: 400,
+            body: { message: 'Error: invalid params value ' + error.details[0].message}
+        };
+    }
+
+    const { 'tenant-id': tenantId } = params;
+    const { appId } = body;
+    let defaultPrechatCapture;
+
+    if (body.prechatCapture === 'name') {
+        defaultPrechatCapture = [{
+            type: 'text',
+            name: 'name',
+            label: 'Name',
+            placeholder: '',
+            minSize: 1,
+            maxSize: 128,
+        }];
+    } else if (body.prechatCapture === 'email') {
+        defaultPrechatCapture = [{
+            type: 'email',
+            name: 'email',
+            label: 'Email',
+            placeholder: '',
+            minSize: 1,
+            maxSize: 128,
+        }];
+    } else {
+        return {
+            statusCode: 400,
+            body: { message: `Bad request: body.prechatCapture invalid value ${body.prechatCapture}` }
+        }
+    }
+
+    let integration;
+    try {
+        integration = await smooch.integrations.create({
+            appId,
+            props: {
+                type: 'web',
+                brandColor: body.brandColor,
+                originWhiteList: body.originWhiteList,
+                businessName: body.businessName,
+                businessIconUrl: body.businessIconUrl,
+                fixedIntroPane: body.fixedIntroPane,
+                integrationOrder: [],
+                prechatCapture: { enabled: true, fields: defaultPrechatCapture },
+                conversationColor: body.conversationColor,
+                backgroundImageUrl: body.backgroundImageUrl,
+                actionColor: body.actionColor,
+                displayStyle: body.displayStyle,
+                buttonWidth: body.buttonWidth,
+                buttonHeight: body.buttonHeight,
+                buttonIconUrl: body.buttonIconUrl
+            }
+        });
+
+    } catch (error) {
+        console.error(JSON.stringify(error));
+        return {
+            statusCode: error.response.status || 500,
+            body: { message: `An Error has occurred trying to create a web integration for tenant ${tenantId}`, error }
+        };
+    }
+
+    const params = {
+        TableName: `${AWS_REGION}-${ENVIRONMENT}-smooch`,
+        Item: {
+            'tenant-id': tenantId,
+            id: integration.integration._id,
+            'app-id': appId,
+            type: 'web',
+            name: body.name,
+            description: body.description
+        }
+    };
+    try {
+        await docClient.put(params).promise();
+    } catch (error) {
+        console.error(JSON.stringify(error));
+
+        return {
+            statusCode: error.statusCode || 500,
+            body: { message: `An Error has occurred trying to save a record in DynamoDB for tenant ${tenantId}`, error }
+        };
+    }
+
+    return {
+        statusCode: 201,
+        body: { integration }
+    };
+};
---
+++ delete-smooch-web-integration/index.js
@@ -0,0 +1,113 @@
+/**
+ * Lambda that deletes an smooch web integration
+ **/
+
+const SmoochCore = require('smooch-core');
+const AWS = require('aws-sdk');
+const smooch = new SmoochCore({
+    keyId: 'act_5da0b5671c42610010a7f245',
+    secret: '6-cyVcqWS185_zvDFGITUPAz1gM1IggG286IKSoEQFZNuDRonU7SZDOv3wOWiFzeMLUUQOijZXeD2BxGY7hhFw',
+    scope: 'account'
+});
+const Joi = require('@hapi/joi');
+
+AWS.config.update({ region: process.env.AWS_REGION || 'us-east-1' });
+const docClient = new AWS.DynamoDB.DocumentClient();
+const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const paramsSchema = Joi.object({
+    'tenant-id': Joi.string()
+        .required()
+        .guid(),
+    id: Joi.string()
+        .required(),
+});
+
+exports.handler = async (event) => {
+    console.log('delete-smooch-web-integration' , JSON.stringify(event));
+    console.log('delete-smooch-web-integration' , JSON.stringify(process.env));
+
+    const { params } = event;
+
+    try {
+       await paramsSchema.validateAsync(params);
+    } catch(error){
+        console.error('Error: invalid params value ' + error.details[0].message);
+        
+        return {
+            statusCode: 400,
+            body: { message: 'Error: invalid params value ' + error.details[0].message}
+        };
+    }
+
+    const { 'tenant-id': tenantId, id: integrationId } = params;
+
+    const queryParams = {
+        Key: {
+         'tenant-id': {
+           S: tenantId
+          }, 
+         'id': {
+           S: integrationId
+        }, 
+        TableName:  `${AWS_REGION}-${ENVIRONMENT}-smooch`
+       }
+    }
+    let appId;
+
+    try {
+       const queryResponse = await ddb.getItem(queryParams).promise();
+        if(queryResponse.Item){
+            appId = queryResponse.Item['app-id'];
+        } else {
+            console.error(`An Error has occurred trying to fetch an app for tenant ${tenantId} and integrationId ${integrationId}`);
+            
+            return {
+                statusCode: 400,
+                body: { message: `An Error has occurred trying to fetch an app for tenant ${tenantId} and integrationId ${integrationId}`, deleted: false}
+            };
+        }
+    } catch (error) {
+        console.error(JSON.stringify(error));
+
+        return {
+            statusCode: error.statusCode || 500,
+            body: { message: `An Error has occurred trying to fetch an app in DynamoDB for tenant ${tenantId} and integrationId ${integrationId}`, deleted: false, error}
+        };
+    }
+
+    try {
+        await smooch.integrations.delete(appId, integrationId);
+    } catch (error) {
+        console.error(JSON.stringify(error));
+
+        return {
+            statusCode: error.response.status || 500,
+            body: { message: `An Error has occurred trying to delete an web integration for tenant ${tenantId} and integrationId ${integrationId}`, deleted: false, error }
+        };
+    }
+
+    const deleteParams = {
+        TableName: `${AWS_REGION}-${ENVIRONMENT}-smooch`,
+        Key: {
+            'tenant-id': tenantId,
+            id: integrationId,
+        }
+    };
+
+    try {
+        await docClient.delete(deleteParams).promise();
+    } catch (error) {
+        console.error(JSON.stringify(error));
+
+        return {
+            statusCode: error.statusCode || 500,
+            body: { message: `An Error has occurred trying to delete a record in DynamoDB for tenant ${tenantId} and integrationId ${integrationId}`, deleted: false, error}
+        };
+    }
+
+    return {
+        statusCode: 200,
+        body: { message: `The web integration with for tenant ${tenantId} and integrationId ${integrationId} has been deleted successfully`, deleted: true }
+    };
+   
+};
