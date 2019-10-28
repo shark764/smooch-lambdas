f6291d33fd74510458dc22bc4147678d22f2a69e
Josh Clowater <joshclowater@users.noreply.github.com>
2019-10-28 11:42:49 -0300

    Revert Jenkins scripts to be compatible with node 5. (#54)

--- install.js
+++ install.js
@@ -1,21 +1,22 @@
-const fs = require('fs');
-const { resolve } = require('path');
-const { join } = require('path');
-const cp = require('child_process');
-const os = require('os');
+
+var fs = require('fs')
+var resolve = require('path').resolve
+var join = require('path').join
+var cp = require('child_process')
+var os = require('os')
 
 // get library path
-const lib = resolve(__dirname);
+var lib = resolve(__dirname)
 console.log(lib);
 fs.readdirSync(lib)
-  .forEach((mod) => {
-    const modPath = join(lib, mod);
-    // ensure path has package.json
-    if (!fs.existsSync(join(modPath, 'package.json'))) return;
+    .forEach(function (mod) {
+        var modPath = join(lib, mod)
+        // ensure path has package.json
+        if (!fs.existsSync(join(modPath, 'package.json'))) return
 
-    // npm binary based on OS
-    const npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm';
+        // npm binary based on OS
+        var npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm'
 
-    // install folder
-    cp.spawn(npmCmd, ['i'], { env: process.env, cwd: modPath, stdio: 'inherit' });
-  });
+        // install folder
+        cp.spawn(npmCmd, ['i'], { env: process.env, cwd: modPath, stdio: 'inherit' })
+    })
--- zip.js
+++ zip.js
@@ -1,16 +1,19 @@
-const fs = require('fs');
-const { resolve } = require('path');
-const { join } = require('path');
-const cp = require('child_process');
+var fs = require('fs');
+var resolve = require('path').resolve;
+var join = require('path').join;
+var cp = require('child_process');
+var os = require('os');
+
 
 // get library path
-const lib = resolve(__dirname);
+var lib = resolve(__dirname);
 fs.readdirSync(lib)
-  .forEach((mod) => {
-    const modPath = join(lib, mod);
-    // ensure path has package.json
-    if (!fs.existsSync(join(modPath, 'package.json'))) return;
+    .forEach(function (mod) {
 
-    // install folder
-    cp.execSync(`zip -r ${mod} *`, { cwd: modPath });
-  });
+        var modPath = join(lib, mod);
+        // ensure path has package.json
+        if (!fs.existsSync(join(modPath, 'package.json'))) return;
+
+        // install folder
+        cp.execSync(`zip -r ${mod} *`, { cwd: modPath });
+    });
