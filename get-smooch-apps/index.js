ffbc3be7f652bdc1c7d15d42216a8cb27f41acef
Irvin Sandoval <irvandoval@gmail.com>
2019-11-11 14:34:01 -0600

    fix issues (#77)

--- get-smooch-apps/index.js
+++ get-smooch-apps/index.js
@@ -36,7 +36,7 @@ exports.handler = async (event) => {
   const { 'tenant-id': tenantId } = params;
   const queryParams = {
     TableName: `${AWS_REGION}-${ENVIRONMENT}-smooch`,
-    KeyConditionExpression: '#tenantId = :t, #integrationType = :type',
+    KeyConditionExpression: '#tenantId = :t and #integrationType = :type',
     IndexName: 'tenant-id-type-index',
     ExpressionAttributeNames: {
       '#tenantId': 'tenant-id',
